8a74ee8241a94f22cacacf4f4633424a459029a7
babakv80 <babakv80@gmail.com>
2017-05-22 13:27:27 +0800

    callPackageMethod() removeed

--- Person.java
+++ Person.java
@@ -2,10 +2,4 @@ public class Person {
 	public static void main(String[] arg){
 		System.out.println("I am a person");
 	}
-	
-	public void callPackageMethod(){
-		HelloWorld hw = new HelloWorld();
-		hw.packageMethod();
-		hw.printMessage("adfasdf");
-	}
 }
